4e8226eda220d12fb3e30c856a20ad16f6860b03
ahmedalaameng <ahmedalaa.m.eng@gmail.com>
2019-06-10 20:56:04 +0200

    some drivers

---
+++ Mared/Headers_macros/headers.h
@@ -0,0 +1,23 @@
+﻿/*
+ * headers.h
+ *
+ * Created: 08/09/2018 02:40:54 م
+ *  Author: Dell
+ */ 
+
+
+#ifndef HEADERS_H_
+#define HEADERS_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 12000000ul
+#include <util/delay.h>
+#define SETBIT(REG,BIT) (REG |= (1 << BIT))
+#define CLRBIT(REG,BIT) (REG &=~ (1 << BIT))
+#define TOGBIT(REG,BIT) (REG ^= (1 << BIT))
+#define READBIT(REG,BIT) ((REG >> BIT)&1)
+
+
+
+#endif /* HEADERS_H_ */
